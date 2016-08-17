17a47ee3a1dafc14c91f384b7fd2a25212e40c8d
calyr <calyr.software@gmail.com>
2016-08-17 10:16:17 -0400

    Cuentas

---
+++ Budget/CuentaTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CuentaTableViewCell.swift
+//  Budget
+//
+//  Created by Roberto Carlos Callisaya Mamani on 8/17/16.
+//  Copyright © 2016 calyr. All rights reserved.
+//
+
+import UIKit
+
+class CuentaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lbTitle: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Budget/ListaTableViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ListaTableViewController.swift
+//  Budget
+//
+//  Created by Roberto Carlos Callisaya Mamani on 8/17/16.
+//  Copyright © 2016 calyr. All rights reserved.
+//
+
+import UIKit
+
+
+let menuData: [String] = ["Gastos","Facturas","Ingresos","Presupuesto","Cuentas"]
+
+class ListaTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableViewCuentas: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return menuData.count
+    }
+    
+    
+     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCellWithIdentifier("Celda", forIndexPath: indexPath)
+        //let cell = tableView.dequeueReusableCellWithIdentifier("Celdacuenta", forIndexPath: indexPath) as! CuentaTableViewCell
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("CellIdentifier", forIndexPath: indexPath) as! CuentaTableViewCell
+        print( indexPath.row )
+        
+                cell.lbTitle.text? = menuData[indexPath.row]
+        
+        //cell.imgMenuTitle.image = UIImage(named: menuDataIcon[indexPath.row])
+        
+        
+        // Configure the cell...
+        
+        return cell
+    }
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+     if editingStyle == .Delete {
+     // Delete the row from the data source
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+     } else if editingStyle == .Insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
